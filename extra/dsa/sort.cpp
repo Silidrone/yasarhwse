ea87139f6a4d7ff6b49bc9cf83ef1581e4109868
Muhamed Cicak <silidrone@gmail.com>
2022-12-25 21:48:13 +0300

    mergesort finished

--- extra/dsa/sort.cpp
+++ extra/dsa/sort.cpp
@@ -83,6 +83,44 @@ int binary_search(int *arr, int x, int low, int high) {
     }
 }
 
+void merge(int *arr, int start, int mid, int end) {
+    int l_len = mid - start + 1;
+    int r_len = end - mid;
+    
+    int l[l_len], r[r_len];
+    
+    for(int i = 0; i < l_len; i++)
+        l[i] = arr[start + i];
+    for(int i = 0; i < r_len; i++)
+        r[i] = arr[mid + 1 + i];
+    
+    int l_index = 0, r_index = 0;
+    int i = start;
+    while(l_index < l_len && r_index < r_len) {
+        if(l[l_index] <= r[r_index]) {
+            arr[i++] = l[l_index++];
+        } else {
+            arr[i++] = r[r_index++];
+        }
+    }
+    
+    while(l_index < l_len) {
+        arr[i++] = l[l_index++];
+    }
+    
+    while(r_index < r_len) {
+        arr[i++] = r[r_index++];
+    }
+}
+
+void merge_sort(int *arr, int low, int high) {
+    if(low > high) return;
+    int med = (low + high) / 2;
+    merge_sort(arr, low, med - 1);
+    merge_sort(arr, med + 1, high);
+    merge(arr, low, med, high); 
+} 
+
 void print(int *input, int size) {
     bool first = true;
     for(int i = 0; i < size; i++) {
@@ -94,12 +132,13 @@ void print(int *input, int size) {
 }
 
 int main() {
-    int data[] = {7, 15, 27, 322, 1888, 9000, 21300, 21311, 21321, 32211, 32212};
+    int data[] = {3, 2, 5, 1, 4, 15000};
     int size = sizeof(data) / sizeof(int);
     print(data, size);
     //selection_sort(data, size);
     //bubble_sort(data, size);
     //insertion_sort(data, size);
-    std::cout << binary_search(data, 32211, 0, size - 1) << std::endl;
-    //print(data, size);
+    //std::cout << binary_search(data, 32211, 0, size - 1) << std::endl;
+    merge_sort(data, 0, size - 1); 
+    print(data, size);
 }
